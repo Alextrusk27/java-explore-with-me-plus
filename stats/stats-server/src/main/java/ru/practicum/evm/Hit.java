e333b00b3db03ef0fbf2591d0e0617a2b715bb59
Fsmnza <140049580+Fsmnza@users.noreply.github.com>
2025-12-20 17:37:50 +0500

    исправила чекстайл

--- stats/stats-server/src/main/java/ru/practicum/evm/Hit.java
+++ stats/stats-server/src/main/java/ru/practicum/evm/Hit.java
@@ -4,19 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Entity
 public class Hit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String app;
     private String uri;
     private String ip;
     private LocalDateTime timestamp;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+
     public Long getId() {
         return id;
     }
