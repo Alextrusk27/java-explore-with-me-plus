2fcb892a218a693649b74f88a107d146daa29fe5
munuprig <egorprygun@ya.ru>
2025-12-31 03:45:27 +0700

    Добавление категорий 2.0.16

--- main-service/src/main/java/ru/practicum/ewm/exception/NotFoundCategoryException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.ewm.exception;
-
-public class NotFoundCategoryException extends RuntimeException {
-    public NotFoundCategoryException(String message) {
-        super(message);
-    }
-}
